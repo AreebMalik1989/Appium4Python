721edba4fd199643c66596e08f4514daf301426a
AreebMalik1989 <areeb.malik1989@gmail.com>
2020-04-22 21:18:11 +0530

    Create run_mode_autotest.py

---
+++ Bulbasaur/run_mode_autotest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import requests
+
+
+def main():
+    url = 'http://127.0.0.1:8886/run?mode=autotest'
+    response = requests.get(url)
+    res_json = response.json()
+    print(res_json)
+
+
+if __name__ == '__main__':
+    main()
